eb1f63097ea884a3c2264ca0e9136e8b1d2bcb7f
DevThiagoCBM <thiago999011@gmail.com>
2026-06-09 16:13:38 -0300

    Create ex25.js

---
+++ ex25/ex25.js
@@ -0,0 +1,6 @@
+//Crie um array com 5 notas de um aluno. Use um laço for para percorrer todos os itens do array, somar todas as notas e depois calcular e mostrar a média final desse aluno.
+var notas = [8, 8 , 9 , 7.5, 10]
+let soma = 0
+for (let indice_soma = 0; indice_soma < notas.length; indice_soma++) {
+     += notas[indice_soma]
+}
